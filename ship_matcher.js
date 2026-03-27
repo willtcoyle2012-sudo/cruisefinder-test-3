f3955aa0e8d2ac960c0512f83a6e2705ead47ff3
William <willtcoyle2012@gmail.com>
2026-03-27 21:52:04 +0000

    Added Nieuw Statendam

--- ship_matcher.js
+++ ship_matcher.js
@@ -6,6 +6,13 @@ const ships = [
     size: "Medium",
     amenities: ["Pools", "Spa", "Shows", "Bars"]
   },
+   {
+    name: "Nieuw Statendam",
+    budget: "Luxury",
+    vibes: ["Relaxation", "Adventure"],
+    size: "Medium",
+    amenities: ["Bars", "Spa", "Shows", "Bars"]
+  },
   {
     name: "MSC Divina",
     budget: "Mid",
